5b449dbf21f81c4bfe00e9fdb27d4d64712c7ada
HailerSH <hsernah.dev@gmail.com>
2021-08-26 23:01:22 -0500

    Creation of the 'login' activity

---
+++ app/src/main/java/com/dcps/neosia/ui/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.dcps.neosia.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.dcps.neosia.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usernameInput = findViewById(R.id.username_input);
+        passwordInput = findViewById(R.id.password_input);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
+        getSupportActionBar().hide();
+    }
+
+    private void login() {
+        Intent mainActivity = new Intent(this, MainActivity.class);
+
+        startActivity(mainActivity);
+        finish();
+    }
+}
